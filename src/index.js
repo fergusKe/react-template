778a72a073c301b582e26d72d46dc25099e18095
fergusKe <fergus.jingtai@gmail.com>
2018-02-10 19:09:27 +0800

    Add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import './css/base.scss';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { AppContainer } from 'react-hot-loader';
+
 fetch('/api/comments/show?id=4199740256395164&page=1')
 	.then(res => res.json())
 	.then(data => {
 		console.log('data = ', data)
 	})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById('root'),
+	)
+}
+
+render(App);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  })
+}
